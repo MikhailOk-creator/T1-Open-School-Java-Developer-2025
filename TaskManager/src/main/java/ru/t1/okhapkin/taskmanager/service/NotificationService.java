384de3f6e6ed78e8ab39db44929a5510f26d0f23
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2025-03-02 19:24:42 +0300

    Added method to send emails The settings for accessing the email account are set. And also added a method for sending to another mail.

--- TaskManager/src/main/java/ru/t1/okhapkin/taskmanager/service/NotificationService.java
+++ TaskManager/src/main/java/ru/t1/okhapkin/taskmanager/service/NotificationService.java
@@ -2,6 +2,9 @@ package ru.t1.okhapkin.taskmanager.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +14,31 @@ public class NotificationService {
 
     private final Logger log = LoggerFactory.getLogger(NotificationService.class);
 
+    @Value("${spring.mail.username}")
+    private String emailForm;
+
+    private final JavaMailSender mailSender;
+
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     public void newMessages(List<String> messages) {
-        messages.forEach(message -> log.info("New message: {}", message));
+        messages.forEach(message ->
+                sendEmail(emailForm, "Task update", message)
+        );
+    }
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(emailForm);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+        log.info("Notification send to mail: {}", to);
     }
 
 }
